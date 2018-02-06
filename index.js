4aada14685dbe93116f50f75181385ff8854fe95
Warmor <grishin.pd@gmail.com>
2018-02-06 22:57:03 +0300

    Исправил замечания

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
-switch (process.argv[2]) {
-  case undefined:
-    console.log(`
-Привет пользователь! 
-Эта программа будет запускать сервер «Кексобукинг». Автор: Гришин Павел.    
-    `);
-    process.exit(0);
-    break;
-  case `--version`:
-    console.log(`v0.0.1`);
-    process.exit(0);
-    break;
-  case `--help`:
-    console.log(`
-Доступные команды:
---help    — печатает этот текст;
---version — печатает версию приложения;
-    `);
-    process.exit(0);
-    break;
-  default:
-    console.log(`
-Неизвестная команда ${process.argv[2]}.
-Чтобы прочитать правила использования приложения, наберите "--help"    
-    `);
-    process.exit(1);
-    break;
-}
+const VERSION_APP = 'v0.0.1';
+const AUTHOR_NAME = 'Павел Гришин';
 
+let getErrorMessage = function (val) {
+  return `Неизвестная команда: "${val}".\nЧтобы прочитать правила использования приложения, наберите "--help"`
+};
+
+let messages = {
+  '--version': VERSION_APP,
+  '--help': `Доступные команды:\n--help    — печатает этот текст;\n--version — печатает версию приложения;`,
+  '': `Привет пользователь!\nЭта программа будет запускать сервер «Кексобукинг». Автор: ${AUTHOR_NAME}.`,
+};
+
+let getMessage = function (key) {
+  return {
+    message: messages[key] ? messages[key] : getErrorMessage(key),
+    exitMode: messages[key] ? 0 : 1,
+  };
+};
+
+let showMessage = function (arrParams) {
+  let dataMessage = getMessage(arrParams[0] ? arrParams[0] : '');
+  console.log(dataMessage.message);
+  process.exit(dataMessage.exitMode);
+
+};
+showMessage(process.argv.slice(2));
